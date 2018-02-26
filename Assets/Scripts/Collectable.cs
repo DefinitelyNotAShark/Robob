710d7eef7aaea2a193b37b868a3dd593a8e6f8d2
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-02-25 21:10:52 -0600

    fixing little abnormalities

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    //needs to spawn in spawnpoints
+    //needs to rotate after been spawned
+    //needs to be able to be collected
+    //needs to destroy once collected
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        System.Console.Write("YOU FUCKIN HIT IT BRAH");
+        Destroy(this);
+    }
+}
